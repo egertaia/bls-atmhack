099220ca1ba42e127bfa32a91d3d1ec67d7e0b82
egertaia <aiaegert@gmail.com>
2021-08-31 13:22:28 +0300

    fix issues

--- server/server.lua
+++ server/server.lua
@@ -1,10 +1,42 @@
+robbedLocations = {}
+
+function isNearSearchedLocation(coords)
+    for k, v in pairs(robbedLocations) do
+        local distanceFromLocation = #(v - coords)
+        if distanceFromLocation < 4 then
+            return true
+        end
+    end
+
+    return false
+end
+
 RegisterServerEvent("bls-atmhack:hack-success")
 AddEventHandler("bls-atmhack:hack-success", function(progressPercentage)
     local src = source
     local user = exports["bls-base"]:getModule("Player"):GetUser(src)
-    local moneyToGive = math.floor(progressPercentage/100) * math.random(BLSE.MinMoney, BLSE.MaxMoney)
+    local userCoords = exports['bls-position']:getPlayerCoords(src)
+    local moneyToGive = progressPercentage / 100 * math.random(BLSE.MinMoney, BLSE.MaxMoney)
 
     user:addMoney(moneyToGive)
-    TriggerClientEvent('DoLongHudText', src, string.format( "Sa varastasid masinast kokku $%d.", moneyToGive), 1)
-
+    TriggerClientEvent("DoLongHudText", src, string.format( "Sa varastasid masinast kokku $%d.", moneyToGive), 1)
 end)
+
+RegisterNetEvent("bls-atmhack:server:attempt-hack")
+AddEventHandler("bls-atmhack:server:attempt-hack", function()
+    local src = source
+    local userCoords = exports['bls-position']:getPlayerCoords(src)
+
+    local isNear = isNearSearchedLocation(userCoords)
+    if (isNear) then
+        TriggerClientEvent("DoLongHudText", src, "Seda masinat on alles hiljuti tühjendatud.", 2)
+    else
+        TriggerClientEvent("bls-atmhack:start", src)
+        table.insert(robbedLocations, userCoords)
+    end
+end)
+
+-- bloki lokatsiooni atm
+-- kas saab häkkida?
+-- securityred vaja majasse juurde panna
+
--- shared/config.lua
+++ shared/config.lua
@@ -1,4 +1,6 @@
-BLSE.atms = {'prop_atm_01', 'prop_atm_02', 'prop_atm_03', 'prop_fleeca_atm', 'v_5_b_atm1', 'v_5_b_atm2'}
+BLSE = {}
+
+BLSE.atms = {"prop_atm_01", "prop_atm_02", "prop_atm_03", "prop_fleeca_atm", "v_5_b_atm1", "v_5_b_atm2"}
 
 BLSE.MinMoney = 2500
 BLSE.MaxMoney = 5000
